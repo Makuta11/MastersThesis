5ca5ddcd037b93550c38c1bc457e56f3bcaf14ea
Makuta11 <christopher.boe.jensen@gmail.com>
2021-11-08 19:16:42 +0100

    update

--- DataProcessing/main.py
+++ DataProcessing/main.py
@@ -1,18 +1,42 @@
 import os
-import mne
 import sys
+import time
 import numpy as np
 import pandas as pd
 
-from src.utils_nback import collective_performace_change, gen_nback_frame, gen_timestamp_frame
+from src.generate_feature_vector import decompress_pickle, compress_pickle
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 def main(bool):
-    dataDir = "/Users/DG/Documents/PasswordProtected/Speciale Outputs/"
-    df = gen_nback_frame(dataDir)
-    TS = gen_timestamp_frame(dataDir)
-    collective_performace_change(df,full=bool)
+    print("Loading Dataset")
+    t = time.time()
+    data = decompress_pickle("/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/face_space_dict_disfa.pbz2")
+    labels = decompress_pickle("/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/disfa_labels.pbz2")
 
-    return df, TS
+    data_list = np.vstack(np.array(list(data.items()))[:,1])
+    y = labels.drop["ID"].to_numpy()
+
+    #import pdb; pdb.set_trace()
+    
+    print(f"Data loaded in {time.time() - t} seconds")
+
+    print("Starting fit")
+    t1 = time.time()
+    clf = MultiOutputClassifier(KNeighborsClassifier(), n_jobs=-1).fit(X,y)
+    clf.predict(X[-2:])
+    print(f"Model fit in {time.time() - t1} seconds")
+
+    print("This is a prediction")
+    clf.predict(X[-5:])
+    print(y[-5:])
+
+    print("Activate debugger for inspection of data")
+    #import pdb;pdb.set_trace()
+
 
 if __name__ == "__main__":
-    main(False)
+    print("starting script")
+    main(True)
+
--- DataProcessing/src/dataloader.py
+++ DataProcessing/src/dataloader.py
@@ -18,10 +18,20 @@ def compress_pickle(title: str, data):
 def load_data(user_train_val, user_test):
     #TODO: Make sure dictionary keys are accessible for training
     dataset = decompress_pickle(f'/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/face_space_dict_disfa.pbz2')
-    
+
 
 class ImageTensorDatasetEpoch(data.Dataset):
     
-    def __init__(self, image_data, label):
-        self.user_id = df[]
+    def __init__(self, data, labels):
+        self.user_id = df["ID"]
+        self.label = df.drop["ID"]
+        self.label_AU = self.label[(self.label >= 1)] = 1
+        self.data = data
+
+    def __len__(self):
+        return self.labels.shape[0]
+    
+    def __getitem__(self, key):
+        data = self.data[key]
+        return slef.data[key], self.label[key]
         
