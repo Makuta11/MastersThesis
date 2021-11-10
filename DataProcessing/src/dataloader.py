7d052b448107ab4a3ed305c3f883f07d18081318
Christopher Ravn Boe Jensen <DG@ChristohersMBP6.localdomain>
2021-11-10 09:42:50 +0100

    local update

--- DataProcessing/src/dataloader.py
+++ DataProcessing/src/dataloader.py
@@ -1,10 +1,11 @@
-import torch
+import torch, bz2, sys
 import torch.nn as nn
 from torch.utils import data
 import torch.nn.functional as F
 import torch.optim as optim
 import pickle
 import numpy as np
+import pandas as pd
 
 def decompress_pickle(file: str):
     data = bz2.BZ2File(file, 'rb')
@@ -15,13 +16,25 @@ def compress_pickle(title: str, data):
     with bz2.BZ2File(title + '.pbz2', 'w') as f: 
         pickle.dump(data, f)
 
+def checkpoint_save(model, save_path, epoch):
+    f = os.path.join(save_path, 'checkpoint-{:06d}.pth'.format(epoch))
+    if 'module' in dir(model):
+        torch.save(model.module.state_dict(), f)
+    else:
+        torch.save(model.state_dict(), f)
+    print('saved checkpoint:', f)
+
 def load_data(user_train_val, user_test):
-    dataset = decompress_pickle(f'/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/face_space_dict_disfa.pbz2')
-    labels = decompress_pickle("/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/disfa_labels.pbz2")
+    if sys.platform == "linux":
+        dataset = decompress_pickle(f'/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/face_space_dict_disfa_test.pbz2')
+        labels = decompress_pickle("/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/disfa_labels_test.pbz2")
+    else:
+        dataset = decompress_pickle(f'/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/face_space_dict_disfa_test.pbz2')
+        labels = decompress_pickle("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/disfa_labels_test.pbz2")
 
     # Initialize parameters
     bad_idx = []
-    data_list = list(data.items())
+    data_list = list(dataset.items())
     data_arr = np.array(data_list)
     
     # Collect bad inputs
@@ -30,7 +43,7 @@ def load_data(user_train_val, user_test):
         try:
             if len(arr) != ln:
                 bad_idx.append(i)
-        else:
+        except:
             bad_idx.append(i)
 
     # Delete bad inputs
@@ -40,30 +53,44 @@ def load_data(user_train_val, user_test):
     # Construct final data arrays
     data_arr = np.vstack(data_arr[:,1])
 
-    labels_test = pd.concat([labels[(labels.ID==te)] for te in user_test])
-    labels_train_val = pd.concat([labels[(labels.ID==tr)] for tr in user_train_val])
+    if sys.platform == "linux":
+        labels_test = pd.concat([labels[(labels.ID==te)] for te in user_test])
+        labels_train_val = pd.concat([labels[(labels.ID==tr)] for tr in user_train_val])
+    else:
+        labels_test = labels.iloc[:100]
+        labels_train_val = labels.iloc[100:]
 
     test_idx = list(labels_test.index)
-    train_idx = list(labels_train.index)
+    train_idx = list(labels_train_val.index)
 
-    data_test = data_arr[test_idx, 1]
-    data_train = data_arr[train_idx, 1]
+    data_test = data_arr[test_idx, :]
+    data_train = data_arr[train_idx, :]
 
-    return data_test, data_train, labels_test.reset_index(drop=True), labels_train.reset_index(drop=True)
+    return data_test, data_train, labels_test.reset_index(drop=True), labels_train_val.reset_index(drop=True)
 
 class ImageTensorDatasetMultitask(data.Dataset):
     
     def __init__(self, data, labels):
-        self.user_id = df["ID"]
-        self.label = df.drop["ID"]
-        self.label_AU = self.label[(self.label >= 1)] = 1
         self.data = data
-
+        self.user_id = labels["ID"]
+        self.labels = labels.drop(columns = "ID")
+        
     def __len__(self):
         return self.labels.shape[0]
     
     def __getitem__(self, key):
         data = self.data[key]
-        label = self.label[key]
-        return slef.data[key], self.label[key]
+        
+        # Multi-label classification labels
+        """ Think this is a mistake. I might try later
+        AUs = list(self.labels.iloc[key][self.labels.iloc[key] != 0].index)
+        if len(AUs) == 0:
+            AUs = 0
+        """
+        AUs = self.labels.iloc[key]
+        AUs[AUs =! 0] = 1
+
+        AU_intensity = self.labels.iloc[key]
+
+        return slef.data[key], AUs.values, AU_intensity.values
         
--- DataProcessing/src/models.py
+++ DataProcessing/src/models.py
@@ -15,15 +15,27 @@ class Multitask(nn.Module):
             nn.BatchNorm1d(data.shape[0]),
             nn.ReLU(),
             nn.Dropout(p = dropout_prop),
-            nn.Linear(in_features = data.shape[0], out_features = 2*12),
+            nn.Linear(in_features = data.shape[0], out_features = fc_hidden_dim_1),
             nn.BatchNorm1d(fc_hidden_dim_1),
             nn.ReLU(),
             nn.Dropout(p = dropout_prop),
             nn.Linear(in_features = fc_hidden_dim_1, out_features = fc_hidden_dim_2)
-            nn.BatchNorm1d(fc_hidden_dim),
+            nn.BatchNorm1d(fc_hidden_dim_2),
             nn.ReLU(),
             nn.Dropout(p = dropout_prop),
-            nn.Linear(in_features = fc_hidden_dim_2, out_features = num_classes)
+            nn.Linear(in_features = fc_hidden_dim_2, out_features = fc_hidden_dim_3)
+            nn.BatchNorm1d(fc_hidden_dim_3),
+            nn.ReLU(),
+            nn.Dropout(p = dropout_prop),
+            nn.Linear(in_features = fc_hidden_dim_3, out_features = fc_hidden_dim_4)
+            nn.BatchNorm1d(fc_hidden_dim_4),
+            nn.ReLU(),
+            nn.Dropout(p = dropout_prop),
+            nn.Linear(in_features = fc_hidden_dim_4, out_features = fc_hidden_dim_5)
+            nn.BatchNorm1d(fc_hidden_dim_5),
+            nn.ReLU(),
+            nn.Dropout(p = dropout_prop),
+            nn.Linear(in_features = fc_hidden_dim_5, out_features = num_AU)
         )
         
         self.sigm = nn.Sigmoid()
@@ -58,25 +70,26 @@ class Multitask(nn.Module):
         batch_size = data.shape[0]
 
         #fully connected layer for multi-label classification (level 1: 1 group)
-        X = self.sigm(self.fc_layer(data))
+        X = self.fc_layer(data)
         
         #fulle connecetd layers for multi-class classification (level 2: 12 groups)
-        X_AU1 = self.fc_layer(data)
-        X_AU2 = self.fc_layer(data)
-        X_AU4 = self.fc_layer(data)
-        X_AU5 = self.fc_layer(data)
-        X_AU6 = self.fc_layer(data)
-        X_AU9 = self.fc_layer(data)
-        X_AU12 = self.fc_layer(data)
-        X_AU15 = self.fc_layer(data)
-        X_AU17 = self.fc_layer(data)
-        X_AU20 = self.fc_layer(data)
-        X_AU25 = self.fc_layer(data)
-        X_AU26 = self.fc_layer(data)
+        
+        intensity_list = [X_AU1 = self.fc_layer(data),
+                          X_AU2 = self.fc_layer(data),
+                          X_AU4 = self.fc_layer(data),
+                          X_AU5 = self.fc_layer(data),
+                          X_AU6 = self.fc_layer(data),
+                          X_AU9 = self.fc_layer(data),
+                          X_AU12 = self.fc_layer(data),
+                          X_AU15 = self.fc_layer(data),
+                          X_AU17 = self.fc_layer(data),
+                          X_AU20 = self.fc_layer(data),
+                          X_AU25 = self.fc_layer(data),
+                          X_AU26 = self.fc_layer(data)]
 
 
         #TODO this needs to be extended to supper hierarchical multi-task multi-label learning
-        return F.softmax(X_stop, dim = 1), F.softmax(X_mode, dim = 1), F.softmax(X_purpose, dim = 1)
+        return [self.sigm(X), intensity_list]
 
 class MultiTaskLossWrapper(nn.Module):
     def __init__(self, model, task_num = 1 + 12):
@@ -87,28 +100,16 @@ class MultiTaskLossWrapper(nn.Module):
 
     def forward(self, data, label, device):
 
-        out_stop, out_mode, out_purpose = self.model(X_img)
-
-        # pull indexes for each individual action unit for multitask hierarchical learning
-        for i, au in enumerate(label.columns):
-            # example from previous code - rewrie with for-loop
-            #purpose_idx = (label[:,2]>=0)
-            #mode_idx = (label[:,1]>=0)
-            au_idx[au] = (label[au] > 0)
+        out_AU, out_AU_intensities = self.model(data)
             
-        stop_loss = F.cross_entropy(out_stop, label[:, 0], weight= self.model.stop_weights)
+        AU_loss = F.binary_cross_entropy(out_AU, label, weight= self.model.stop_weights) #TODO: add class weight
         loss_collect = torch.exp(-self.log_sigmas[0])*stop_loss + self.log_sigmas[0]/2
 
-        if purpose_idx.sum() > 0:
-          purpose_loss = F.cross_entropy(out_purpose[purpose_idx], label[purpose_idx, 2])
-          loss_collect += torch.exp(-self.log_sigmas[2])*purpose_loss + self.log_sigmas[2]/2
-        else:
-          purpose_loss = torch.Tensor(0).to(device)
-
-        if mode_idx.sum() > 0:
-          mode_loss = F.cross_entropy(out_mode[mode_idx], label[mode_idx, 1])
-          loss_collect += torch.exp(-self.log_sigmas[1])*mode_loss + self.log_sigmas[1]/2
-        else:
-          mode_loss = torch.Tensor(0).to(device)
+        for i, lab in enumerate(label):
+          if lab != 0:
+            au_tmp_loss = F.cross_entropy(out_AU_intensities[i], label[i]) 
+            loss_collect += torch.exp(-self.log_sigmas[i])*au_tmp_loss + self.log_sigmas[0]/12
+          else:
+            au_tmp_loss = torch.Tensor(0).to(device)
 
         return loss_collect, mode_loss, purpose_loss, stop_loss, self.log_sigmas.data.tolist()
