ea14ace068b7945b8a158f746bbe99c4c27d3c28
Makuta11 <christopher.boe.jensen@gmail.com>
2021-11-26 11:04:56 +0100

    update memory multioutput

--- DataProcessing/main_multioutput.py
+++ DataProcessing/main_multioutput.py
@@ -20,8 +20,8 @@ def main(bool):
     print("Loading Dataset")
     t = time.time()
     if sys.platform == 'linux':
-        data = decompress_pickle("/zhome/08/3/117881/MastersThesis/DataProcessing/pickles/face_space_dict_disfa.pbz2")
-        labels = decompress_pickle("/work3/s164272/data/Features/disfa_labels.pbz2")
+        data = decompress_pickle("/work3/s164272/data/Features/face_space_dict_disfa_large1.pbz2")
+        labels = decompress_pickle("/work3/s164272/data/Features/disfa_labels_large1.pbz2")
     else:
         data = decompress_pickle("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/face_space_dict_disfa_test.pbz2")
         labels = decompress_pickle("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/disfa_labels_test.pbz2")
