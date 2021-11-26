00c11cd6d26b423199e1909cbf73e30eb5d4cda4
Makuta11 <christopher.boe.jensen@gmail.com>
2021-11-26 09:17:46 +0100

    main update

--- DataProcessing/main_multitask.py
+++ DataProcessing/main_multitask.py
@@ -93,7 +93,7 @@ for k, BATCH_SIZE in enumerate([16]):
 
     # Training Parameters
     if sys.platform == "linux":
-        EPOCHS = 100
+        EPOCHS = 50
     else:
         EPOCHS = 5
     SAVE_FREQ = 10
@@ -111,8 +111,8 @@ for k, BATCH_SIZE in enumerate([16]):
         os.makedirs(f'{save_path}/{today[:19]}')
 
     # CV testing for LR and DR
-    for i, LEARNING_RATE in enumerate([1e-4]):
-        for j, DROPOUT_RATE in enumerate([.45]):
+    for i, LEARNING_RATE in enumerate([5e-5]):
+        for j, DROPOUT_RATE in enumerate([.5]):
 
             # Name for saving the model
             name = f'Batch{BATCH_SIZE}_Drop{DROPOUT_RATE}_Lr{LEARNING_RATE}'
