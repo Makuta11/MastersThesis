0f384c077a8e4e3638a945dc6d1100816fee7103
Makuta11 <christopher.boe.jensen@gmail.com>
2021-12-01 14:58:37 +0100

    update and new files

--- DataProcessing/hpc_jobs/LLDA.sh
+++ DataProcessing/hpc_jobs/LLDA.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#BSUB -q gpuv100
+#BSUB -q hpc
 #BSUB -J My_Test
 ### number of core
-#BSUB -n 1
+#BSUB -n 24
 ### specify that all cores should be on the same host
-#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "span[hosts=1]"
 ### specify the memory needed
-#BSUB -R "rusage[mem=50GB]"
+#BSUB -R "rusage[mem=15GB]"
 ### Number of hours needed
 #BSUB -W 23:59
 ### added outputs and errors to files
@@ -17,4 +17,4 @@ echo "Runnin script..."
 
 module load cuda/10.2
 module load python3/3.8.11
-python3 main_multitask.py > outputs/multitask_$(date +"%d-%m-%y")_$(date +'%H:%M:%S')
+python3 main_LLDA.py > outputs/LLDA_$(date +"%d-%m-%y")_$(date +'%H:%M:%S')
