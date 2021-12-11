b2e9386877b591ebc409ba466ef4078981b436bf
Makuta11 <christopher.boe.jensen@gmail.com>
2021-12-11 19:47:35 +0100

    kernel computation script

---
+++ DataProcessing/EmotionModel/hpc_jobs/gen_kernel.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#BSUB -q hpc
+#BSUB -J genKERN
+### number of core
+#BSUB -n 1
+### specify that all cores should be on the same host
+#BSUB -R "span[hosts=1]"
+### specify the memory needed
+#BSUB -R "rusage[mem=350GB]"
+### Number of hours needed
+#BSUB -W 23:59
+### added outputs and errors to files
+#BSUB -o outputs/Output_%J.out
+#BSUB -e outputs/Error_%J.err
+
+echo "Runnin script..."
+
+module load python3/3.8.11
+python3 src/generate_kernel.py > outputs/genKERN
+
---
+++ DataProcessing/EmotionModel/src/generate_kernel.py
@@ -0,0 +1,107 @@
+import os, sys
+
+import numpy as np
+import pandas as pd
+
+from utils import decompress_pickle
+from sklearn.metrics.pairwise import rbf_kernel
+
+def compute_kernel(X, settings: dict):
+    print("Computing kernel")
+    if settings['kernel'] == 'linear':
+        K = np.dot(X,X.T) # linear kernel
+    elif settings['kernel'] == 'rbf':
+        K = rbf_kernel(X, gamma=settings['gamma']) # gamma defaults to 1/n_features
+    else:
+        print('Invalid kernel option, terminating')
+        exit()
+    print('.. finished')
+    return K
+
+def load_data_for_kernel(subset=None):
+    if subset:
+        if sys.platform == "linux":
+            # Big dataload on hpc
+            dataset = np.load('/work3/s164272/data/Features/face_space_dict_disfa_large_subset.npy', allow_pickle=True)
+            labels = decompress_pickle("/work3/s164272/data/Features/disfa_labels_large1.pbz2")
+            misses = np.load('/work3/s164272/data/Features/misses_disfa_large_subset.npy', allow_pickle=True)
+            # Unfold dict inside 0-dimensional array (caused by np.save/np.load)
+            dataset = dataset.tolist()
+        else:
+            # Small testing dataload for local machine
+            #dataset = decompress_pickle(f'/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/EmotionModel/pickles/face_space_dict_disfa_test_subset.npy')
+            dataset = np.load("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/EmotionModel/pickles/face_space_dict_disfa_large_subset_300_test.npy", allow_pickle=True)
+            labels = decompress_pickle("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/EmotionModel/pickles/disfa_labels_large1.pbz2")
+            misses = np.load('/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/EmotionModel/pickles/misses_disfa_large_subset.npy', allow_pickle=True)
+            # Unfold dict inside 0-dimensional array (caused by np.save/np.load)
+            labels = labels[:4840*6]
+            dataset = dataset.tolist()
+    else:
+        if sys.platform == "linux":
+            # Big dataload on hpc
+            dataset = decompress_pickle('/work3/s164272/data/Features/face_space_dict_disfa_large1.pbz2')
+            labels = decompress_pickle("/work3/s164272/data/Features/disfa_labels_large1.pbz2")
+            misses = decompress_pickle('/work3/s164272/data/Features/misses_disfa_large1.pbz2')
+        else:
+            # Small testing dataload for local machine
+            #dataset = decompress_pickle(f'/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/face_space_dict_disfa_test.pbz2')
+            dataset = np.load("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/EmotionModel/pickles/face_space_dict_disfa_large_subset.npy", allow_pickle=True)
+            labels = decompress_pickle("/Volumes/GoogleDrive/.shortcut-targets-by-id/1WuuFja-yoluAKvFp--yOQe7bKLg-JeA-/EMOTIONLINE/MastersThesis/DataProcessing/pickles/disfa_labels_large1.pbz2")
+   
+   # Remove missed dataindexes from labels (currently not any so its not implemented)
+    if len(misses) > 0:
+        pass
+
+    # Initialize parameters
+    bad_idx = []
+    if type(dataset) == dict:
+        data_list = list(dataset.items())
+        data_arr = np.array(data_list)
+    else:
+        data_arr = dataset
+
+    # Collect bad inputs
+    ln = data_arr[0,1].shape[0]
+    for i, arr in enumerate(data_arr[:,1]):
+        try:
+            if len(arr) != ln:
+                bad_idx.append(i)
+        except:
+            bad_idx.append(i)
+
+    # Delete bad inputs from labels and array
+    labels = labels.drop(bad_idx).reset_index(drop=True)
+    data_arr = np.delete(data_arr, bad_idx, axis=0)
+
+    # Construct final data arrays
+    data_arr = np.vstack(data_arr[:,1])
+    data_arr = np.nan_to_num(data_arr)
+    return data_arr
+
+def save_kernel(K, name):
+    if sys.platform == "linux":
+        save_path = "/work3/s164272/data/assests"
+    else:
+        raise Exception("Please do not try to compute this locally!")
+    
+    np.save(f'{save_path}/{name}_kernel.npy', K)
+
+def main():
+    # Load dataset
+    print("Loading data")
+    dataset = load_data_for_kernel(subset=True)
+    print("Data loaded")
+
+    # Define settings for kernel
+    settings = {'kernel': 'rbf'}
+
+    # Compute kernel
+    print("Computing kernel")
+    K = compute_kernel(dataset, settings)
+    print("Kernal generated")
+
+    # Save kernel to assests folder
+    save_kernel(K, settings['kernel'])
+
+if __name__ == "__main__":
+    main()
