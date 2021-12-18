2c54acac8872b96105968d0dd798f10638dffaa4
Makuta11 <christopher.boe.jensen@gmail.com>
2021-12-18 11:35:07 +0100

    rename test

--- DataProcessing/EmotionModel/src/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-import pickle
-
-df = pickle.load(open("/zhome/08/3/117881/MastersThesis/DataProcessing/EmotionModel/src/assets/subsets", 'rb'))
-
-print(df)
